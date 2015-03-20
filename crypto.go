cdb54f06f20f4d9c66cf042220f2d8f1d5f0e117
Josh Hawn <josh.hawn@docker.com>
2015-03-20 16:46:07 -0700

    Remove unnecessary bits, add ResumableHash
    Docker-DCO-1.1-Signed-off-by: Josh Hawn <josh.hawn@docker.com> (github: jlhawn)

--- crypto.go
+++ crypto.go
@@ -21,36 +21,18 @@ func (h Hash) HashFunc() Hash {
 }
 
 const (
-	MD4       Hash = 1 + iota // import golang.org/x/crypto/md4
-	MD5                       // import crypto/md5
-	SHA1                      // import crypto/sha1
-	SHA224                    // import crypto/sha256
-	SHA256                    // import crypto/sha256
-	SHA384                    // import crypto/sha512
-	SHA512                    // import crypto/sha512
-	MD5SHA1                   // no implementation; MD5+SHA1 used for TLS RSA
-	RIPEMD160                 // import golang.org/x/crypto/ripemd160
-	SHA3_224                  // import golang.org/x/crypto/sha3
-	SHA3_256                  // import golang.org/x/crypto/sha3
-	SHA3_384                  // import golang.org/x/crypto/sha3
-	SHA3_512                  // import golang.org/x/crypto/sha3
+	SHA224 Hash = 1 + iota // import crypto/sha256
+	SHA256                 // import crypto/sha256
+	SHA384                 // import crypto/sha512
+	SHA512                 // import crypto/sha512
 	maxHash
 )
 
 var digestSizes = []uint8{
-	MD4:       16,
-	MD5:       16,
-	SHA1:      20,
-	SHA224:    28,
-	SHA256:    32,
-	SHA384:    48,
-	SHA512:    64,
-	SHA3_224:  28,
-	SHA3_256:  32,
-	SHA3_384:  48,
-	SHA3_512:  64,
-	MD5SHA1:   36,
-	RIPEMD160: 20,
+	SHA224: 28,
+	SHA256: 32,
+	SHA384: 48,
+	SHA512: 64,
 }
 
 // Size returns the length, in bytes, of a digest resulting from the given hash
@@ -63,11 +45,24 @@ func (h Hash) Size() int {
 	panic("crypto: Size of unknown hash function")
 }
 
-var hashes = make([]func() hash.Hash, maxHash)
+// ResumableHash is the common interface implemented by all resumable hash
+// functions.
+type ResumableHash interface {
+	// ResumableHash is a superset of hash.Hash
+	hash.Hash
+	// Len returns the number of bytes written to the Hash so far.
+	Len() int64
+	// State returns a snapshot of the state of the Hash.
+	State() ([]byte, error)
+	// Restore resets the Hash to the given state.
+	Restore(state []byte) error
+}
 
-// New returns a new hash.Hash calculating the given hash function. New panics
+var hashes = make([]func() ResumableHash, maxHash)
+
+// New returns a new ResumableHash calculating the given hash function. New panics
 // if the hash function is not linked into the binary.
-func (h Hash) New() hash.Hash {
+func (h Hash) New() ResumableHash {
 	if h > 0 && h < maxHash {
 		f := hashes[h]
 		if f != nil {
@@ -85,58 +80,9 @@ func (h Hash) Available() bool {
 // RegisterHash registers a function that returns a new instance of the given
 // hash function. This is intended to be called from the init function in
 // packages that implement hash functions.
-func RegisterHash(h Hash, f func() hash.Hash) {
+func RegisterHash(h Hash, f func() ResumableHash) {
 	if h >= maxHash {
 		panic("crypto: RegisterHash of unknown hash function")
 	}
 	hashes[h] = f
 }
-
-// PublicKey represents a public key using an unspecified algorithm.
-type PublicKey interface{}
-
-// PrivateKey represents a private key using an unspecified algorithm.
-type PrivateKey interface{}
-
-// Signer is an interface for an opaque private key that can be used for
-// signing operations. For example, an RSA key kept in a hardware module.
-type Signer interface {
-	// Public returns the public key corresponding to the opaque,
-	// private key.
-	Public() PublicKey
-
-	// Sign signs msg with the private key, possibly using entropy from
-	// rand. For an RSA key, the resulting signature should be either a
-	// PKCS#1 v1.5 or PSS signature (as indicated by opts). For an (EC)DSA
-	// key, it should be a DER-serialised, ASN.1 signature structure.
-	//
-	// Hash implements the SignerOpts interface and, in most cases, one can
-	// simply pass in the hash function used as opts. Sign may also attempt
-	// to type assert opts to other types in order to obtain algorithm
-	// specific values. See the documentation in each package for details.
-	Sign(rand io.Reader, msg []byte, opts SignerOpts) (signature []byte, err error)
-}
-
-// SignerOpts contains options for signing with a Signer.
-type SignerOpts interface {
-	// HashFunc returns an identifier for the hash function used to produce
-	// the message passed to Signer.Sign, or else zero to indicate that no
-	// hashing was done.
-	HashFunc() Hash
-}
-
-// Decrypter is an interface for an opaque private key that can be used for
-// asymmetric decryption operations. For example, an RSA key kept in a hardware
-// module.
-type Decrypter interface {
-	// Public returns the public key corresponding to the opaque,
-	// private key.
-	Public() PublicKey
-
-	// Decrypt decrypts msg. The opts argument should be appropriate for
-	// the primitive used. See the documentation in each implementation for
-	// details.
-	Decrypt(rand io.Reader, msg []byte, opts DecrypterOpts) (plaintext []byte, err error)
-}
-
-type DecrypterOpts interface{}
