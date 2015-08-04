253dbe1a393924f2817224044c9de4d26356fcd3
Pradipta Kumar <pradipta.banerjee@gmail.com>
2015-08-04 12:42:20 +0000

    Add support for building with gccgo compiler

--- sha256/sha256block.go
+++ sha256/sha256block.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build !386,!amd64
+// +build !gc
 
 // SHA256 block step.
 // In its own file so that a faster assembly or C version
